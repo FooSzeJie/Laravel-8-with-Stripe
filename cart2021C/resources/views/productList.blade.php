72b764050371f6eaf8d5bc010d73b67cf2167672
FooSzeJie <D200234C@sc.edu.my>
2021-12-19 23:47:20 +0800

    productList

--- cart2021C/resources/views/productList.blade.php
+++ cart2021C/resources/views/productList.blade.php
@@ -3,16 +3,18 @@
 <div class="row row-cols-1 row-cols-md-3 g-4">
 @foreach($products as $product)
   <div class="col">
-    <div class="card">
+    <br>
+    <div class="card ">
         <input type="hidden" name="id" id="id" value="{{$product->id}}">
         <img src="{{ asset('images/') }}/{{$product->image}}" width="100" class="img-fluid" alt="">
         <div class="card-body">
             <h5 class="card-title">{{$product->name}}</h5>
             <p class="card-text">{{$product->description}}</p>
             <p class="card-text">{{$product->price}}</p>
-            <a href="{{ route('product.detail', ['id' => $product -> id])}}" class="btn btn-warning btn-xs">Edit</a>
+            <a href="{{ route('product.detail', ['id' => $product -> id])}}" class="btn btn-warning btn-xs">Product Detail</a>
       </div>
     </div>
+    <br>
   </div>
   @endforeach
   
